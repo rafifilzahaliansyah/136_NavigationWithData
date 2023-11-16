f0778a69dbc4e506fd8e323c66deeab85190fd83
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2023-11-16 16:07:04 +0700

    Menambah data ui state

--- app/src/main/java/com/example/esjumbo/data/OrderUIState.kt
+++ app/src/main/java/com/example/esjumbo/data/OrderUIState.kt
@@ -1,7 +1,11 @@
 package com.example.esjumbo.data
 
 data class OrderUIState(
-    val jumlah: Int = 0,
-    val rasa: String = "",
-    val harga: String = "",
+val jumlah: Int = 0,
+val rasa: String = "",
+val harga: String = "",
+val nama: String = "",
+val alamat: String = "",
+val noTelp: String = ""
 )
+
